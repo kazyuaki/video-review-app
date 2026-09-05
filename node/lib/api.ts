606ad39a0315caeaf2eb46b0e880677077442a7c
kazyuaki <kazuyama195@gmail.com>
2026-09-05 19:17:44 +0900

    🔧 Laravel APIとの共通通信設定を追加

---
+++ node/lib/api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+  withXSRFToken: true,
+  headers: {
+    Accept: "application/json",
+  },
+});
